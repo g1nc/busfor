4b1013a9e9c56255beecf1adeaf0abed9d6df4ad
Oleg Globin <ginc.xyz@gmail.com>
2019-07-03 22:54:59 +0300

    schedule by day method to helper and views

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,2 +1,12 @@
 module ScheduleHelper
+  def schedule_by_days(trips)
+    result = []
+    @min_date ||= Trip.min_date
+    @max_date ||= Trip.max_date
+    all_dates = (@min_date..@max_date).group_by(&:wday)
+    all_dates.each do |day, dates|
+      result << day if dates.all? { |date| trips.to_a.select { |trip| trip[:start_date] == date }.present? }
+    end
+    result
+  end
 end
